0bf23b7e1593665facfb53e6361cf15eb00df837
JuSellier <juju.sel.mail@gmail.com>
2021-03-29 13:47:04 +0200

    added link to instagram on about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,8 +5,18 @@ import BasePage from '../../components/BasePage/BasePage'
 const About = () => {
   return (
     <BasePage>
+    <div className="About">
       <h1>About NeoCraft</h1>
       <p className="left-line-p">NeoCraft is an initiative from Julien Sellier aimed at promoting artistic usage of technology.</p>
+
+      <h2>Follow the Instagram account</h2>
+      <p className="left-line-p">
+        If you want to discover more algorithmic artworks, feel free to view our instagram.
+        </p>
+        <a className="btn-link" href="https://www.instagram.com/neocraft.art/" rel="noreferrer noopener">
+        Click here to check out NeoCraft on instagram.
+        </a>
+    </div>
     </BasePage>
   )
 }
